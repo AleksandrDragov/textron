cb6afaad4a2579b69d0f5b9d21563542599ec2b3
AleksandrDragov <118523521+AleksandrDragov@users.noreply.github.com>
2024-06-17 17:21:14 +0200

    add validation

--- src/components/BackCall/BackCall.jsx
+++ src/components/BackCall/BackCall.jsx
@@ -1,23 +1,61 @@
 import React from 'react';
 import './BackCall.less';
+import { useForm } from "react-hook-form";
+
 function BackCall() {
+  const { register, 
+    formState: { errors, isValid }, 
+    handleSubmit, 
+    reset 
+  } = useForm({
+    mode: 'onBlur'
+  });
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+    reset();
+  };
+
+  const validateTrim = (value) => {
+    if (value.trim() === '') {
+      return 'This field cannot be empty or contain only spaces';
+    }
+    return true;
+  };
+
   return (
     <div className="backCall-container">
       <h1 className="backCall-title">We will call you in 24 hours</h1>
-      <form className="backCall-form">
+      <form className="backCall-form" onSubmit={handleSubmit(onSubmit)}>
         <div className="backCall-inputWrapper">
-          <input
-            className="backCall-input"
-            type="text"
-            placeholder="write your name"
-          />
-          <input
-            className="backCall-input"
-            type="text"
-            placeholder="write your phone number"
-          />
+          <label className='backCall-label'>
+            <input
+              {...register('name', {
+                required: 'Please enter a name',
+                validate: validateTrim
+              })}
+              className="backCall-input"
+              placeholder="write your name"
+            />
+            {errors?.name && <p className='backCall-errors'>{errors?.name?.message || 'error!'}</p>}
+          </label>
+          <label className='backCall-label'>
+            <input
+              {...register('phone', {
+                required: 'Please enter a phone number',
+                pattern: {
+                  value: /^(?:(?:\+|00)420\s?)?\d{3}\s?\d{3}\s?\d{3}$/,
+                  message: 'Phone number is not valid example(777 777 777)'
+                },
+                validate: validateTrim
+              })}
+              className="backCall-input"
+              placeholder="write your phone number"
+            />
+            {errors?.phone && <p className='backCall-errors'>{errors?.phone?.message || 'error!'}</p>}
+          </label>
         </div>
-        <button className="backCall-button" type="submit">
+        <button className="backCall-button" type="submit" disabled={!isValid}>
           Order call
         </button>
       </form>
@@ -26,3 +64,4 @@ function BackCall() {
 }
 
 export default BackCall;
+
