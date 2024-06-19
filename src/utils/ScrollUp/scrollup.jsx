f7e98999ee12903fd49fb00d380c4884cbf4dda2
AleksandrDragov <118523521+AleksandrDragov@users.noreply.github.com>
2024-06-19 19:16:03 +0200

    tryi

--- src/utils/ScrollUp/scrollup.jsx
+++ src/utils/ScrollUp/scrollup.jsx
@@ -30,7 +30,7 @@ function ScrollUp() {
   return (
     <div className={`scrollUp-container ${isVisible ? 'visible' : ''}`}>
       {isVisible && (
-        <button className="scrollUp-btn" onClick={handleUp}><Icon id='arrow-up' width={30} height={30}/></button>
+        <button className="mail" onClick={handleUp}><Icon id='arrow-up' width={30} height={30}/></button>
       )}
     </div>
   );
