3d27dbca62ebded76268419ddc50390a3067c4e6
AleksandrDragov <118523521+AleksandrDragov@users.noreply.github.com>
2024-06-26 19:55:08 +0200

    try

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -3,7 +3,7 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const sendEmail = (form) => {
-  const { REACT_APP_YOUR_SERVICE_ID, REACT_APP_YOUR_TEMPLATE_ID, REACT_APP_YOUR_PUBLIC_KEY } = process.env;
+  //const { REACT_APP_YOUR_SERVICE_ID, REACT_APP_YOUR_TEMPLATE_ID, REACT_APP_YOUR_PUBLIC_KEY } = process.env;
 
 
   return emailjs.sendForm('textronSender_1273871', 'template_hn61si5', form, 'TUnl3P7C8CsqQpe-4')
