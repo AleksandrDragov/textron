b2bc4697edeee59e2679f82ce1849d9563398062
AleksandrDragov <118523521+AleksandrDragov@users.noreply.github.com>
2024-06-30 19:28:15 +0200

    PUSH

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -2,11 +2,12 @@ import emailjs from '@emailjs/browser';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-require('dotenv').config();
+import { configDotenv } from 'dotenv';
+
 const sendEmail = (form) => {
   const { REACT_APP_SERVICE_ID, REACT_APP_TEMPLATE_ID, REACT_APP_PUBLIC_KEY } = process.env;
   
-  return emailjs.sendForm(REACT_APP_SERVICE_ID, REACT_APP_TEMPLATE_ID, form, REACT_APP_PUBLIC_KEY)
+  return emailjs.sendForm('REACT_APP_SERVICE_ID', REACT_APP_TEMPLATE_ID, form, REACT_APP_PUBLIC_KEY)
   .then(
       () => {
           toast.success("send", {
@@ -14,8 +15,10 @@ const sendEmail = (form) => {
             });
         },
         (error) => {
+          console.log(REACT_APP_PUBLIC_KEY, REACT_APP_SERVICE_ID,REACT_APP_TEMPLATE_ID)
             toast.error("not send ", {
             position:"top-center",
+            
           });
       }
     );
