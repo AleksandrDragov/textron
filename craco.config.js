edf57e38ed24b48f9a352a61208ba1dc23ea917b
AleksandrDragov <118523521+AleksandrDragov@users.noreply.github.com>
2024-06-30 19:00:34 +0200

    update mailer

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,30 @@
 const CracoLessPlugin = require('craco-less');
+const webpack = require('webpack');
 
 module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        buffer: require.resolve('buffer/'),
+        process: require.resolve('process/browser'),
+        path: require.resolve('path-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify')
+      };
+
+      webpackConfig.plugins = [
+        ...webpackConfig.plugins,
+        new webpack.ProvidePlugin({
+          Buffer: ['buffer', 'Buffer'],
+          process: 'process/browser'
+        })
+      ];
+
+      return webpackConfig;
+    },
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
@@ -13,6 +37,4 @@ module.exports = {
       },
     },
   ],
- 
-  };
-
+};
